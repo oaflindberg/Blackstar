2de5f863bc79e172c353b60a145fc7b7ec33a39e
DOM <48492083+AltDom@users.noreply.github.com>
2019-11-18 22:59:31 +0100

    First push

---
+++ script.js
@@ -0,0 +1,27 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const logo = document.querySelector('.logo');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index)=> {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.4s ease forwards ${index / 7 + 0.2}s`;
+            }
+        });
+
+        //Burger Animation
+        burger.classList.toggle('toggle1');
+        logo.classList.toggle('toggle2');
+    });
+    
+}
+
+navSlide();

47484cea6f74f017234559f4f0712999dc850d9a
DOM <48492083+AltDom@users.noreply.github.com>
2019-11-26 21:03:03 +0100

    merch page

--- merch.js
+++ merch.js
@@ -3,6 +3,7 @@
  
 var counter = 0,
 $items = document.querySelectorAll('.carousel figure'),
+$labels = document.querySelectorAll('h6')
 numItems = $items.length;
  
 var showCurrent = function(){
@@ -10,7 +11,11 @@ var showCurrent = function(){
     [].forEach.call($items, function(el){
         el.classList.remove('show');
     });
+    [].forEach.call($labels, function(el){
+        el.classList.remove('show');
+    });
     $items[itemToShow].classList.add('show');
+    $labels[itemToShow].classList.add('show');
 };
  
 $next = document.querySelector('.next');
